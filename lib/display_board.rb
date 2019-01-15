9944e4940fac90ff0c8368220ed10285ebf243e2
Sean Eastlestson <neurotrashman@protonmail.com>
2019-01-15 19:45:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+horizontal_line = ["-----------"]
+Row = ["   " ,  "|" , "   " ,  "|" , "   " ,  "|" ,]
+
+Def display_board 
+puts Row
+puts horizontal_line
+puts Row
+puts horizontal_line
+puts Row
+puts horizontal_line
+end
